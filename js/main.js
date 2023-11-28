b39b5ec53b43526bc1fd39404da627f69533aa27
Tim Dolov <dolov-t@mail.ru>
2023-11-28 19:51:08 +0500

    updated

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import {getPictures} from './data.js';
 import {renderGallery} from './gallery.js';
+import './load-picture.js';
+import './form.js';
 
 const pictures = getPictures();
 renderGallery(pictures);
